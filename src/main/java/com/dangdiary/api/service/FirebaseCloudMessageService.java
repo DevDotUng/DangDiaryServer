e0a350d680427cc9b2244e50ffc6965119839286
ung <wkdu4510@gmail.com>
2023-09-13 14:20:14 +0900

    [수정] List.of가 java 8 버전에서는 사용 불가능해 Arrays.asList로 변경

--- src/main/java/com/dangdiary/api/service/FirebaseCloudMessageService.java
+++ src/main/java/com/dangdiary/api/service/FirebaseCloudMessageService.java
@@ -14,6 +14,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -58,7 +59,7 @@ public class FirebaseCloudMessageService {
 
         GoogleCredentials googleCredentials = GoogleCredentials
                 .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
-                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+                .createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
 
         googleCredentials.refreshIfExpired();
         return googleCredentials.getAccessToken().getTokenValue();
