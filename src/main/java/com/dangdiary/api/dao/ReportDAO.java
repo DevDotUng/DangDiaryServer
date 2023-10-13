1fc0a3b16f7bc5c21524021a6a699ae1feda5c65
ung <wkdu4510@gmail.com>
2023-10-13 19:37:54 +0900

    [신규] 신고내역 기능 구현

--- src/main/java/com/dangdiary/api/dao/ReportDAO.java
+++ src/main/java/com/dangdiary/api/dao/ReportDAO.java
@@ -1,5 +1,6 @@
 package com.dangdiary.api.dao;
 
+import com.dangdiary.api.dto.report.ReportHistoryDTO;
 import com.dangdiary.api.dto.report.ReportRequest;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -8,4 +9,5 @@ public interface ReportDAO {
     void report(ReportRequest request);
     boolean deleteReport(Long reportId);
     boolean updateReport(ReportRequest request);
+    ReportHistoryDTO getReportHistory(int userId);
 }
--- src/main/java/com/dangdiary/api/service/report/ReportService.java
+++ src/main/java/com/dangdiary/api/service/report/ReportService.java
@@ -1,11 +1,12 @@
 package com.dangdiary.api.service.report;
 
 import com.dangdiary.api.dto.report.ReportDTO;
+import com.dangdiary.api.dto.report.ReportHistoryDTO;
 import com.dangdiary.api.dto.report.ReportRequest;
 
 public interface ReportService {
      ReportDTO report(ReportRequest request);
      boolean deleteReport(Long reportId);
      boolean updateReport(ReportRequest request);
-
+     ReportHistoryDTO getReportHistory(int userId);
 }
